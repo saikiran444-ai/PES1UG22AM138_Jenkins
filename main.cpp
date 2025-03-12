64e763f94ad852f937f8b0647c86f8ca3fe4860d
saikiran444-ai <skiran17777@gmail.com>
2025-03-13 00:23:29 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "Change\n";
+  cout << "Change1\n";
+  return 0;
+}
